40304bf31a377857eb7fcbf01daa2703c104652d
xavi-b <developer@xavi-b.fr>
2020-04-24 18:20:59 +0200

    Corrections and improvements

--- src/plugin.cpp
+++ src/plugin.cpp
@@ -15,11 +15,6 @@ KU::PLUGIN::PluginVersion KU_Camera_Plugin::version() const
     return { 1, 0, 0 };
 }
 
-QSet<KU::PLUGIN::PluginInfo> KU_Camera_Plugin::dependencies() const
-{
-    return QSet<KU::PLUGIN::PluginInfo>();
-}
-
 QString KU_Camera_Plugin::license() const
 {
     return "LGPL";
@@ -30,7 +25,7 @@ QIcon KU_Camera_Plugin::icon() const
     return QIcon();
 }
 
-bool KU_Camera_Plugin::initialize(const QSet<KU::PLUGIN::PluginInterface*>& plugins)
+bool KU_Camera_Plugin::initialize()
 {
     return true;
 }
